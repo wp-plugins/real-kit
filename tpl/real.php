96d8df7878c2a5fd6d3881fe50c541f530ce5cfd
real.master <real.master@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 14:43:46 +0000

    Fix 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/real-kit/trunk@1121425 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/real.php
@@ -0,0 +1,17 @@
+<style>
+  .donate {
+    display: inline-block;
+    padding: 10px 30px;
+    text-decoration: none;
+    background: #88DBF7;
+    border: 1px solid silver;
+    -webkit-border-radius: 5px;
+       -moz-border-radius: 5px;
+            border-radius: 5px;
+  }
+</style>
+<h1><?= __('Donation', 'realkit'); ?></h1>
+<p><?= __('I would be very grateful for any support!', 'realkit'); ?></p>
+<a target="_blank" href="https://funding.webmoney.ru/realplugins-for-wordpress/donate" class="button button-primary">
+  <?= __('Donate', ''); ?>
+</a>
